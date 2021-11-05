0cbb5713f2ee7a1214aa6842da35407120803ae7
Simon Kaine <simonkaine@gmail.com>
2021-11-04 17:31:21 -0700

    snapshot test passing for Article List.

---
+++ src/components/app/articles/ArticleList.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ArticleList from './articleList';
+
+const testArticle = [{
+    author: "Ryan Lawyer", 
+    title: "What does Zillow’s exit tell us about the health of the iBuying market?"
+}, 
+{
+    author: "Connie Loizos", 
+    title: "An accelerator and fund says it's giving away its voting rights to scientist founders: \"They lose control\""
+}];
+
+describe('Tests for articles array', () => {
+    it('Should render articles array', () => {
+        const { asFragment } = render(<ArticleList articles={testArticle}/>);
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
